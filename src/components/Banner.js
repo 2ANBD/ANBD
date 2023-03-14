d7f21776c3117fd4c1b9368b666c271e657ab35a
Foccy <pouneun@naver.com>
2023-03-14 18:05:04 +0900

    carasoul

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "../scss/Style.scss";
-
+import { Carousel } from "antd";
+// const contentStyle = {
+//   margin: "0px",
+//   height: "160px",
+//   color: "#fff",
+//   lineHeight: "160px",
+//   textAlign: "center",
+//   background: "#364d79"
+// };
 const Banner = () => {
-  return <div className="h1">Banner</div>;
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+  return (
+    <Carousel afterChange={onChange}>
+      <div>
+        <h3 className="contentStyle">1</h3>
+      </div>
+      <div>
+        <h3 className="contentStyle">1</h3>
+      </div>
+      <div>
+        <h3 className="contentStyle"></h3>
+      </div>
+      <div>
+        <h3 className="contentStyle"></h3>
+      </div>
+    </Carousel>
+  );
 };
-
 export default Banner;

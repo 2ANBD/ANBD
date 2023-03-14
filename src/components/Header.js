f39acbe35f85e8447e603715debae08233dd3b12
jungsm <seonmunjeong95@ggmail.com>
2023-03-15 03:46:11 +0900

    230315 헤더 완성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
 import menu from './img/menu.png';
 import logo from './img/main-logo.png';
+import alarm from './img/alarm.png';
 import React from "react";
+import { SearchOutlined } from "@ant-design/icons";
 import "../scss/Style.scss";
 
 function Header() {
   return (
     <section className="header">
       <div className="header_gnb">
-      <img className="header_gnb_menu" src={menu} alt=""></img>
-      <img className="header_gnb_logo" src={logo} alt=""></img>
+      <div className="header_gnb_menu">
+        <img src={menu} alt=""></img>
+      </div>
+      <div className="header_gnb_logo">
+        <img src={logo} alt=""></img>
+      </div>
+      <div className="header_gnb_alarm">
+      <img src={alarm} alt=""></img>
+       </div>
+      </div>
+      <div className="header_search">
+        <input type="search" value="원하시는 상품을 검색해보세요"></input>
       </div>
     </section>
   );
 }
 export default Header;
+

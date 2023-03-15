1643cb1a1947b23e6b8ddfc7dab2990eb57196d4
hyeyeong12 <127397294+hyeyeong12@users.noreply.github.com>
2023-03-15 11:14:15 +0900

    230315헤더외형구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
+import menu from './img/menu.png';
+import logo from './img/main-logo.png';
+import alarm from './img/alarm.png';
 import React from "react";
 import "../scss/Style.scss";
 
 function Header() {
-  return <div className="header">헤더 선문</div>;
+  return (
+    <section className="header">
+      <div className="header_gnb">
+      <div className="header_gnb_menu">
+        <img src={menu} alt=""></img>
+      </div>
+      <div className="header_gnb_logo">
+        <img src={logo} alt=""></img>
+      </div>
+      <div className="header_gnb_alarm">
+      <img src={alarm} alt=""></img>
+       </div>
+      </div>
+      <div className="header_search">
+        <input type="search" value="원하시는 상품을 검색해보세요"></input>
+      </div>
+    </section>
+  );
 }
 export default Header;

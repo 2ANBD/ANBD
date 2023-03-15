5a9735a0687f69f3237eabe0c50103287739d941
KimJunHo98 <dydgnscc6845@gmail.com>
2023-03-15 17:41:32 +0900

    230315 김준호 section 컴포넌트 작성

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import "./scss/Style.scss";
 // import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
-import Section from "./components/Section";
+import Section1 from "./components/Section1";
+import Section2 from "./components/Section2";
+import Section3 from "./components/Section3";
+import Section4 from "./components/Section4";
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -10,7 +13,10 @@ export default function App() {
     <div className="wrap">
       <Header />
       <Banner />
-      <Section />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
       <Footer />
     </div>
   );

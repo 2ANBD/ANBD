c113c579ab73ced12781a178adbe4f178c0692b8
Foccy <pouneun@naver.com>
2023-03-16 12:52:17 +0900

    0316 김종건 banner2추가 +  이너, 배경색수정

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
+import Banner2 from "./components/Banner2";
 import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
 import Footer from "./components/Footer";
@@ -17,6 +18,7 @@ export default function App() {
       <Banner />
       <Section1 />
       <Section2 />
+      <Banner2 />
       <Section3 />
       <Section4 />
       <Footer />

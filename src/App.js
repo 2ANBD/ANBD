a6e46bb4f37d596dbc4f7d7bb6d1c1b71a91dfdc
Foccy <edlee127127@gmail.com>
2023-03-17 18:03:19 +0900

    0317 김종건 uploadpage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./scss/Style.scss";
-import { Routes, Route, Link} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FloatButton from "./components/FloatBtn";
 import BottomNav from "./components/BottomNav";
 import MainPage from "./components/MainPage.js";
+import UploadPage from "./components/UploadPage.js";
 
 export default function App() {
   return (
     <div className="wrap">
       <Header />
       <Routes>
-				<Route path="/" element={<MainPage />}></Route>
+				{/* <Route path="/" element={<MainPage />}></Route> */}
+				<Route path="/" element={<UploadPage />}></Route>
 				{/* <Route path="/ProductPage/:id" element={<ProductPage />}></Route> */}
-				{/* <Route path="/UploadPage" element={<UploadPage />}></Route> */}
 			</Routes>
-      <MainPage />
 
       <Footer />
       <FloatButton />

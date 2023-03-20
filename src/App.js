77b50a046506e504256da75f2a5fe2af48f6c6a0
Foccy <pouneun@naver.com>
2023-03-20 13:05:35 +0900

    0320

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import Footer from "./components/Footer";
 import FloatButton from "./components/FloatBtn";
 import BottomNav from "./components/BottomNav";
 import MainPage from "./components/MainPage.js";
-import UploadPage from "./components/UploadPage.js";
+import UploadImage from "./components/UploadImage.js";
 
 export default function App() {
   return (
     <div className="wrap">
       <Header />
       <Routes>
-				<Route path="/" element={<MainPage />}></Route>
-				{/* <Route path="/" element={<UploadPage />}></Route> */}
+				{/* <Route path="/" element={<MainPage />}></Route> */}
+				<Route path="/" element={<UploadImage />}></Route>
 				{/* <Route path="/ProductPage/:id" element={<ProductPage />}></Route> */}
 			</Routes>
-
       <Footer />
       <FloatButton />
       <BottomNav />

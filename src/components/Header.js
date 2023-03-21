6961f313b66811b0418057176140a4c2d4a7a16e
was7894 <was7894@nate.com>
2023-03-21 17:13:47 +0900

    modify(20230321)-김형섭- 헤더 푸터 버튼 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 // import menu from './img/menu.png';
 // import logo from './img/main-logo.png';
 // import alarm from './img/alarm.png';
+import { Button, Space } from "antd";
 import React from "react";
 import "../scss/Style.scss";
-import {BellOutlined , SearchOutlined} from "@ant-design/icons";
+import { BellOutlined, SearchOutlined } from "@ant-design/icons";
 
 function Header() {
   return (
@@ -11,9 +12,13 @@ function Header() {
       <div id="container">
         <div className="inner">
           <div className="content_wrap">
-            <BellOutlined />
-            <img src="/images/icons/img_header_logo.png" alt=""className="logo"/>
-            <SearchOutlined />
+            <Button size="large">
+              <BellOutlined />
+            </Button>
+            <img src="/images/icons/img_header_logo.png" alt="" className="logo" />
+            <Button size="large">
+              <SearchOutlined />
+            </Button>
           </div>
         </div>
       </div>

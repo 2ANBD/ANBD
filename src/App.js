d1358348477ae184508a57f89eb18acae1447f61
jungsm <seonmunjeong95@gmail.com>
2023-03-22 16:45:47 +0900

    네이버 로그인 구현

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ import Footer from "./components/Footer";
 import FloatButton from "./components/FloatBtn";
 import BottomNav from "./components/BottomNav";
 import PaymentPage from "./components/PaymentPage";
+import LoginPage from "./components/LoginPage";
 import UploadImage from "./components/UploadImage";
-
 
 export default function App() {
   return (
     <div className="wrap">
       <Header />
       <Routes>
-				<Route path="/" element={<MainPage />}></Route>
-				<Route path="/products" element={<ProductPage />}></Route>
-				<Route path="/payment" element={<PaymentPage />}></Route>
-				<Route path="/UploadImage" element={<UploadImage />}></Route>
-			</Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/products" element={<ProductPage />}></Route>
+        <Route path="/payment" element={<PaymentPage />}></Route>
+        <Route path="/loginPage" element={<LoginPage />}></Route>
+        <Route path="/UploadImage" element={<UploadImage />}></Route>
+      </Routes>
       <Footer />
       <FloatButton />
       <BottomNav />

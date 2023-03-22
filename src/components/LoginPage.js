0558fadc8d565bd9d419bb54bd09452db2122ceb
ihkim8 <sarah.ih.kim8@gmail.com>
2023-03-22 14:51:36 +0900

    로그인 구현

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,23 +7,23 @@ function LoginPage() {
   const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <h1>
-              <a href={KAKAO_AUTH_URL}>
-                <img src="./images/Loginbtn/kakao.png" />
-              </a>
-            </h1>
-          </Route>
-          <Route path="/oauth/kakao/callback">
-            <MainPage />
-          </Route>
-        </Switch>
+  return ( 
+    <div id="container">
+     <div className="inner">
+      <div className="login_wrap">
+      <div className="header">
+        <span>모두를 위한 쇼핑몰 </span>
       </div>
-    </Router>
+      <div className="logo_box">
+      <img src="/images/icons/img_main.png" alt="" />
+      </div>
+        <ul className="login_btn">
+          <li className="kakao_btn" ><a href={KAKAO_AUTH_URL}>카카오 계정으로 로그인</a></li>
+          <li className="naver_btn"><a href={KAKAO_AUTH_URL}>네이버 계정으로 로그인</a></li>
+         </ul>
+      </div>
+    </div>
+    </div>
   );
 }
 

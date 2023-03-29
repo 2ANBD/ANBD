299ca6748eeb2f7d819b57fb352a0b037b2738b3
was7894 <was7894@nate.com>
2023-03-29 15:41:23 +0900

    API주소 수정 TEST페이지 주석처리

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,10 +2,10 @@
 // export const API_URL = "https://port-0-mango-sever-6g2llfc5dt1p.sel3.cloudtype.app";
 
 /* 본서버 */
-// export const API_URL = "https://port-0-anbd-db-sever-6g2llfcbue07.sel3.cloudtype.app";
+export const API_URL = "https://port-0-anbd-db-sever-6g2llfcbue07.sel3.cloudtype.app";
 
 /* 로컬서버 */
-export const API_URL = "http://localhost:8080";
+// export const API_URL = "http://localhost:8080";
 
 /* 삼항연산자 */
 /* export const API_URL = process.env.NODE_ENV === "production" ? "https://port-0-anbd-db-sever-6g2llfcbue07.sel3.cloudtype.app" : "http://localhost:8080"; */

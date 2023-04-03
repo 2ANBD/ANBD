ddc9d78eb2f3f3254161238e9057028a10ee5892
KimJunHo98 <dydgnscc6845@gmail.com>
2023-04-03 12:01:09 +0900

    230403 김준호 product페이지3 price 수정

--- src/components/ProductPage3.js
+++ src/components/ProductPage3.js
@@ -130,7 +130,7 @@ const ProductPage3 = () => {
                                                             {product.price === null ? null : <li className="price">{product.price}원</li>}
                                                         </ul>
                                                         <ul className="product_text_center">
-                                                            {product.brand === null ? null : <li className="brand"><span>{product.brand}</span></li>}{product.price === null ? null : <li className="price">{product.price}원</li>}
+                                                            {product.brand === null ? null : <li className="brand"><span>{product.brand}</span></li>}
                                                             {product.size === null ? null : <li className="size"><span>{product.size}</span></li>}
                                                         </ul>
                                                         <div className="product_text_bottom">

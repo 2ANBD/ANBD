869d97c86353081215c6cbac8d8b7d33ca2d2b27
Jsm <seonmunjeong95@gmail.com>
2023-04-07 15:39:34 +0900

    필요없는 페이지 삭제

--- src/components/LoginPage2.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const LoginPage2 = () => {
-  const code = new URL(window.location.href).searchParams.get("code");
-  return <div>{code}</div>;
-};
-
-export default LoginPage2;
